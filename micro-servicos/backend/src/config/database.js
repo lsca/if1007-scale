9213d657859016eb845786a2b07c6fcb293dfca6
Matheus Raz <mrol@cin.ufpe.br>
2018-06-21 19:13:03 -0300

    inserção da porta do mongo e alteração para uso do MongoClient

--- micro-servicos/backend/src/config/database.js
+++ micro-servicos/backend/src/config/database.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect(`mongodb://mongo-todo/todo`)
+module.exports = mongoose.connect(`mongodb://localhost:27017/todo`, {
+    useMongoClient: true
+});
--- micro-servicos/backend/src/config/server.js
+++ micro-servicos/backend/src/config/server.js
@@ -1,4 +1,4 @@
-const port = 8080
+const port = 3000
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
@@ -8,8 +8,11 @@ server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
 server.use(allowCors)
 
-server.listen(port, function(){
-    console.log(`BACKEND RUNNING @ ${port}`);
+server.listen(port,  (err) => {
+    if(err) {
+       return console.log(`Erro ao tentar se conectar ao servidor`, err);
+    }
+    console.log(`Backend rodando na porta ${port}`);
 })
 
 module.exports = server
